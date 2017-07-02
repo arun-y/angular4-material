8a158771648af0831efc7567dc60b360ffa67094
Arun Y <arunwizz@gmail.com>
2017-07-02 21:31:03 +0530

    the very basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MdGridListModule } from '@angular/material';
+import { MdListModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MdGridListModule, MdListModule, MdInputModule, BrowserAnimationsModule, MdCheckboxModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
